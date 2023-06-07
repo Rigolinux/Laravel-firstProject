3b6066a7e6f11291aa5c600f030acd8619106c8b
rigolinux <rigoberto7567@gmail.com>
2023-06-07 15:52:45 -0600

    learning laravel migrations

---
+++ database/migrations/2023_06_07_210912_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255)->nullable(false);
+            $table->string('slug', 255)->nullable(false);
+            $table->text('content');
+            $table->string('image');
+            $table->enum("posted", ["yes", "no"])->default("no");
+            $table->timestamps();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
