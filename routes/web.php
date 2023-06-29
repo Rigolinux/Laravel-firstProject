97745c01c61eafa3ae26a7d38dfbc17ba8e4524b
rigolinux <rigoberto7567@gmail.com>
2023-06-29 15:51:31 -0600

    breezze initialization

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Dashboard\PostController;
-use App\Http\Controllers\Dashboard\CategoryController;
-use App\Models\Category;
-use Illuminate\Support\Facades\App;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,86 +14,18 @@ use Illuminate\Support\Facades\App;
 |
 */
 
-
-//create route for dashboard all routes
-// this case is more easy to use but we need to use all the routes
-
-
-//route test with middleware
-Route::middleware([App\Http\Middleware\Testmiddleware::class])->group(function(){
-    Route::get('/test', function () {
-        return view('welcome');
-    });
-
-});	
-
-
-// routes bt prefix
-Route::group(['prefix' => 'dashboard'], function () {
-   // Route::resource('post', PostController::class);
-   //Route::resource('category', CategoryController::class);
-
-   // you can concat the resource 2 coulb be like this
-    Route::resource([
-        'post' => PostController::class,
-        'category' => CategoryController::class
-    ]);
-
-    // and i can elimite route with only or except
-    //Route::resource('post', PostController::class)->only(['index','show']);
-    //Route::resource('post', PostController::class)->except(['index','show']);
-    
-});
-
-
-// create a group of routes
-/* Route::controller(PostController::class)->group(function(){
-    Route::get('post', [PostController::class, 'index'])->name('post.index');
-    Route::get('post/{post}', [PostController::class, 'show'])->name('post.show');
-    Route::get('post/create',[PostController::class, 'show'])->name('post.create');
-    Route::post('post',[PostController::class, 'store'])->name('post.store');
-    Route::put('post/{post}',[PostController::class, 'update'])->name('post.update');
-    Route::delete('post/{post}',[PostController::class, 'update'])->name('post.delete');
-}) */
-
-
-//create route for dashboard all routes includes in one resource its like the above
-// but we can use only the routes that we need or we can use all of them
-
-/* Route::get('post', [PostController::class, 'index'])->name('post.index');
-Route::get('post/{post}', [PostController::class, 'show'])->name('post.show');
-Route::get('post/create',[PostController::class, 'show'])->name('post.create');
-Route::post('post',[PostController::class, 'store'])->name('post.store');
-Route::put('post/{post}',[PostController::class, 'update'])->name('post.update');
-Route::delete('post/{post}',[PostController::class, 'update'])->name('post.delete');
- */
-
-
-//Route::get("/test",[TestController::class,'test']);
-
-
-//Route::get('/', [\App\Http\Controllers\TextControler:: class,'index']);
-/*
-Route::get("/test",[TestController::class,'test']);
-
 Route::get('/', function () {
-    return view('welcome2');
-});
-
-Route::get('/hello', function () {
     return view('welcome');
-})->name('hello');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
-Route::get('/custom',function() {
-    $custom = 'Custom Message';
-    return view('custom',['msj'=>$custom]);
 });
 
-*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
+require __DIR__.'/auth.php';
